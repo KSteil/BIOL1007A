d145ff4062f09eecf49f2934e82c1adbc7546171
KSteil <ksteil@middlebury.edu>
2023-01-12 13:44:19 -0500

    Added  Assignment 3

---
+++ 2023-01-12-Scripting-in-R.R
@@ -0,0 +1,16 @@
+##### Programming in R   
+##### 12 January 2023   
+##### SS
+
+# Advantages
+## interactive use
+## graphics, statistics
+## very active community of contributors
+## works on multiple platforms
+
+# Disadvantages
+## lazy evaluation
+## some packages are poorly documented
+## some functions are hard to learn (steep learning curve)
+## unreliable packages 
+## problems with big data (multiple GBs)
