2cf57062fbbc17f1ca252d808e94837777ca2f61
KSteil <ksteil@middlebury.edu>
2023-01-22 20:52:02 -0500

    Weekly assignment 2 and an updated index

--- Notes/2023-01-19-matrices-and-dataframes.R
+++ Notes/2023-01-19-matrices-and-dataframes.R
@@ -49,3 +49,99 @@ str(dataSub)
 ### sort a data frame by values
 orderedIris <- iris[order(iris$Petal.Length),] # dollar sign finds named element
 head(orderedIris)
+
+
+####### FUNCATIONS IN R
+
+# everything in r is a function
+sum(3,2) # sum
+3 + 2 # + sign is a function
+sd # will show the code of a function if you run JUST the argument
+
+### User-defined functions
+#functionName <- function(argX=defualtX, argY=default Y) {
+  ##curly bracket starts the body of the function
+  ### lines of code and notes
+  ### create local variables (only visible to R within the function)
+    #argX<-(smth)
+  ### return(z)
+#}
+
+
+myFunk <- function(a=3, b=4){
+  z <- a + b
+  return(z) #return stops the function at that line 
+}
+
+myFunk() #returns the defaults
+myFunk(a=100, b=3.4) # runs with specified variables
+
+
+myFunkBad <- function(a=3){
+  z <- a + b
+  return(z)
+}
+myFunkBad()#returns an error, object 'b' not found
+b <- 100
+myFunkBad() #can run and recognizes the global value, but BAD practice
+
+myFunkEmpty <- function(a=NULL, b=NULL){
+  z <- a + b
+  return(z)
+}
+myFunkEmpty() #does not return an error, returns 0
+
+print(z) # cannot print z, not defined outside of the function
+
+z <- myFunk() #NOW saves z as the default ran through the function
+
+### Multiple return statements
+
+##############################################################
+# Function: HardyWeinberg
+# input: all allele frequenecy p (0,1)
+# output: p and the frequencies of 3 genotypes AA AB BB
+#-------------------------------------------------------------
+HardyWeinberg <- function(p = runif(1)){
+  if(p>1.0 | p<0){
+    return("Function Failure: p must be between 0 and 1")
+  } ## '|' OR
+  q <- 1 -p
+  fAA <- p^2
+  fAB <-2*p*q
+  fBB <- q^2
+  vecOut <- signif(c(p=p, AA=fAA, AB=fAB, BB=fBB), digits=3) #signif returns significant figures
+  return(vecOut)
+}
+#############################################################
+
+HardyWeinberg()
+freqs <- HardyWeinberg()
+
+HardyWeinberg(p=3) # returns an error, as stated in the function
+
+
+####### Create a more complex default value
+##################################################
+# Function: fitLinear2
+# fits simple regression line
+# input: numeric list (p) of predictor (x) and response (y)
+# outputs: slope and p-value
+
+fitLinear2 <-function(p=NULL){
+  if(is.null(p)){
+    p <- list(x=runif(20), y=runif(20))
+  }
+  myMod <- lm(p$x~p$y)
+  myOut <- c(slope= summary(myMod)$coefficients[2,1],
+             pValue = summary(myMod)$coefficients[2,4])
+  plot(x=p$x, y=p$y) #quick plot to check output
+  return(myOut)
+}
+
+fitLinear2()
+myPars <- list(x=1:10, y=runif(10))
+fitLinear2(p=myPars)
+
+
+
